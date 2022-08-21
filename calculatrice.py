60d2be4c3e5e7af758bc7b65d21864214717cf15
Van <92119774+mvsteven@users.noreply.github.com>
2022-08-21 14:52:59 +0200

    Update calculatrice.py

--- calculatrice.py
+++ calculatrice.py
@@ -4,7 +4,6 @@ second_nombre = input("Entrer un second nombre: ")
 if premier_nombre.isdigit() and second_nombre.isdigit() == True:
     premier_nombre = int(premier_nombre)
     second_nombre = int(second_nombre)
-    print(
-        f"La somme de {premier_nombre} et {second_nombre} vaut: {premier_nombre + second_nombre}")
+    print(f"La somme de {premier_nombre} et {second_nombre} vaut: {premier_nombre + second_nombre}")
 else:
     print("Veuillez entrer des nombre s'il vous plait!")
